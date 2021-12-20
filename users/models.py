dcc8b7edee6537c161786a21724c3e92f6fcbf0d
adnan kaya <adnankaya.py@gmail.com>
2021-12-21 01:48:51 +0300

    ui improvement

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
+from core.models import Base
 
 
 class Profile(models.Model):
@@ -8,3 +9,8 @@ class Profile(models.Model):
 
     class Meta:
         db_table = 't_profile'
+
+
+class SocialAccount(Base):
+    # TODO
+    pass
